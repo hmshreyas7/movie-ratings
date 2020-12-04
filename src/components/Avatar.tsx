a7bcb1817a31ed2500fea6c141c9ca22ccff5b3d
hmshreyas7 <hmshreyas7@gmail.com>
2020-12-04 08:27:52 -0800

    Set avatar image based on sign-in provider

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -2,9 +2,20 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import defaultAvatar from '../assets/default-user-avatar.png';
 import firebase from 'firebase/app';
+import dotenv from 'dotenv';
 
 interface RootState {
   user: firebase.User;
+}
+
+function setAvatarImage(photoURL: string) {
+  if (photoURL.includes('graph.facebook.com')) {
+    dotenv.config();
+    const accessToken = process.env.REACT_APP_FACEBOOK_ACCESS_TOKEN;
+    return photoURL + `?access_token=${accessToken}`;
+  } else {
+    return photoURL;
+  }
 }
 
 function Avatar() {
@@ -12,7 +23,10 @@ function Avatar() {
 
   return (
     <div className='avatar-wrapper'>
-      <img src={user.photoURL ? user.photoURL : defaultAvatar} alt='Avatar' />
+      <img
+        src={user.photoURL ? setAvatarImage(user.photoURL) : defaultAvatar}
+        alt='Avatar'
+      />
       <p>{user.displayName ? user.displayName : 'Guest'}</p>
     </div>
   );
