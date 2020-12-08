b2a5a409b6cf9cfc7c8540abf3cc5d2d2bdbbb0f
hmshreyas7 <hmshreyas7@gmail.com>
2020-12-07 19:47:31 -0800

    Enable standard login with email and password

--- src/components/LoginPage.tsx
+++ src/components/LoginPage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { ChangeEvent, useEffect, useState } from 'react';
 import firebase from 'firebase/app';
 import 'firebase/auth';
 import { useHistory } from 'react-router-dom';
@@ -18,6 +18,10 @@ function facebookSignIn() {
 function LoginPage() {
   let history = useHistory();
   let dispatch = useDispatch();
+  let [loginInfo, setLoginInfo] = useState({
+    email: '',
+    password: '',
+  });
 
   useEffect(() => {
     let unsubscribe = firebase.auth().onAuthStateChanged((user) => {
@@ -31,6 +35,28 @@ function LoginPage() {
       unsubscribe();
     };
   }, [dispatch, history]);
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+
+    setLoginInfo((prevInfo) => {
+      return {
+        ...prevInfo,
+        [name]: value,
+      };
+    });
+  };
+
+  const emailSignIn = () => {
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(loginInfo.email, loginInfo.password);
+
+    setLoginInfo({
+      email: '',
+      password: '',
+    });
+  };
 
   const goToSignUp = () => {
     history.push('/signup');
@@ -48,10 +74,22 @@ function LoginPage() {
           </button>
         </div>
         <div className='standard-login-wrapper'>
-          <input type='email' placeholder='Email' />
-          <input type='password' placeholder='Password' />
+          <input
+            type='email'
+            placeholder='Email'
+            name='email'
+            value={loginInfo.email}
+            onChange={handleChange}
+          />
+          <input
+            type='password'
+            placeholder='Password'
+            name='password'
+            value={loginInfo.password}
+            onChange={handleChange}
+          />
           <div className='standard-login-buttons-wrapper'>
-            <input type='submit' value='Login' />
+            <input type='submit' value='Login' onClick={emailSignIn} />
             <input type='button' value='Sign Up' onClick={goToSignUp} />
           </div>
         </div>
