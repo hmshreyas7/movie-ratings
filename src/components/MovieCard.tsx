2dfade4082c3e77fee2ddd0f478b461dec52b471
hmshreyas7 <hmshreyas7@gmail.com>
2020-12-09 20:18:55 -0800

    Include 'add' option on hover

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -11,6 +11,9 @@ function MovieCard(props: MovieCardProps) {
     <div className='movie-card-wrapper'>
       <div className='movie-card-poster'>
         <img src={props.poster} alt={props.title} />
+        <div className='movie-card-overlay'>
+          <button>Add</button>
+        </div>
       </div>
       <div className='movie-card-info'>
         <p>{props.title}</p>
