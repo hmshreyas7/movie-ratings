ddfe0b7c5b9bded3f714ef9cd30d419485f44916
hmshreyas7 <hmshreyas7@gmail.com>
2021-01-02 10:51:57 -0800

    Fix #10

--- src/components/SideNav.tsx
+++ src/components/SideNav.tsx
@@ -1,16 +1,54 @@
-import React from 'react';
+import { useMediaQuery } from '@material-ui/core';
+import { ArrowDropDown, ArrowDropUp } from '@material-ui/icons';
+import React, { useEffect, useRef, useState } from 'react';
 import Avatar from './Avatar';
 import Home from './Home';
 import NavLinks from './NavLinks';
 import SearchBar from './SearchBar';
 
 function SideNav() {
+  const mediaQueryMatch = useMediaQuery('(max-width: 600px)');
+  let [isExpanded, setExpanded] = useState(false);
+  const menu = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    setExpanded(!mediaQueryMatch);
+    mediaQueryMatch &&
+      document.addEventListener('click', handleMenuClose, { capture: true });
+
+    return () => {
+      mediaQueryMatch &&
+        document.removeEventListener('click', handleMenuClose, {
+          capture: true,
+        });
+    };
+  }, [mediaQueryMatch]);
+
+  const handleMenuToggle = () => {
+    setExpanded((prevValue) => !prevValue);
+  };
+
+  const handleMenuClose = (e: MouseEvent) => {
+    if (!menu.current?.contains(e.target as Node)) {
+      setExpanded(false);
+    }
+  };
+
   return (
-    <div className='side-nav-drawer'>
-      <Home />
-      <Avatar />
-      <SearchBar />
-      <NavLinks />
+    <div className='side-nav-drawer' ref={menu}>
+      <div className='side-nav-logo-toggle-wrapper'>
+        <Home />
+        {mediaQueryMatch && (
+          <div className='side-nav-toggle' onClick={handleMenuToggle}>
+            {isExpanded ? <ArrowDropUp /> : <ArrowDropDown />}
+          </div>
+        )}
+      </div>
+      <div className={isExpanded ? 'side-nav-expanded' : 'side-nav-collapsed'}>
+        <Avatar />
+        <SearchBar />
+        <NavLinks />
+      </div>
     </div>
   );
 }
