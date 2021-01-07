6f1a5df51a19d2b12bea80828d6fcd52f292952f
hmshreyas7 <hmshreyas7@gmail.com>
2021-01-07 01:35:13 -0800

    Fix #14

--- src/components/SideNav.tsx
+++ src/components/SideNav.tsx
@@ -1,6 +1,8 @@
 import { useMediaQuery } from '@material-ui/core';
 import { ArrowDropDown, ArrowDropUp } from '@material-ui/icons';
 import React, { useEffect, useRef, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { RootState } from '../rootState';
 import Avatar from './Avatar';
 import Home from './Home';
 import NavLinks from './NavLinks';
@@ -10,6 +12,7 @@ function SideNav() {
   const mediaQueryMatch = useMediaQuery('(max-width: 600px)');
   let [isExpanded, setExpanded] = useState(false);
   const menu = useRef<HTMLDivElement>(null);
+  const isLoading = useSelector((state: RootState) => state.isLoading);
 
   useEffect(() => {
     setExpanded(!mediaQueryMatch);
@@ -23,6 +26,14 @@ function SideNav() {
         });
     };
   }, [mediaQueryMatch]);
+
+  useEffect(() => {
+    if (mediaQueryMatch && isLoading && menu.current) {
+      (document.querySelector(
+        '.loading-indicator'
+      ) as HTMLElement).style.top = `${menu.current.offsetHeight.toString()}px`;
+    }
+  }, [mediaQueryMatch, isLoading]);
 
   const handleMenuToggle = () => {
     setExpanded((prevValue) => !prevValue);
